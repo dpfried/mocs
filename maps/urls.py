31e6dca1f32bb37d3c5bf140ddd149825cf2e970
Daniel Fried <dfried@email.arizona.edu>
2013-03-01 14:41:02 -0700

    restructured to django 1.3

--- maps/urls.py
+++ maps/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('maps.views',
     url(r'^map/$', 'map'),
-    url(r'^task_status/$', 'task_status'),
+    url(r'^task_status/$', 'task_status', name='task_status'),
     url(r'^task/$', 'get_task'),
 )
