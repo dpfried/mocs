d4d7a4d7a1a9ad9c0cc1c0fcab9e6f3180ed8895
Daniel Fried <dfried@email.arizona.edu>
2013-08-24 18:07:04 +0900

    print timing info

--- lib/status.py
+++ lib/status.py
@@ -1,5 +1,8 @@
+import datetime
+
 def set_status(status, model=None, state='PROGRESS'):
     # task.update_state(state=state, meta={'status': status})
+    print '%s %s %s' % (datetime.datetime.now().time().isoformat(), ':', status)
     if model is not None:
         if model.status is None:
             model.status = status
